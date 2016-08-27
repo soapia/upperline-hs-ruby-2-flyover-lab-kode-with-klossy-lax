6f3071dda6b13487f7e8c0a48e363d1e161cf15d
sofia-o <sofia.ongele@ealas.org>
2016-08-27 20:52:49 +0000

    this was so entertaining

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,62 @@
 ## Code your solution here.
+def ny
+  "we will now be departing the big apple, until next time!!"
+end
+
+def pa
+  "we're flying over pimptastic pensylvania!!"
+end
+
+def oh
+  "we're flying over outstanding ohio!!"
+end
+
+def inn
+  "we're flying over.. um.. indiana.."
+end
+
+def il
+  "we're flying over incomparable illinois!!"
+end
+
+def ms
+  "we're flying over the magical missouri!! karlie would be so proud!!"
+end
+
+def ks
+  "we're flying over krazy kansas!! #{"yeehaw".upcase}"
+end
+
+def co
+  "we're flyong over cooooool colorado, dude."
+end
+
+def az
+  "we're flying over that one rebellious state that refuses to participate in daylight savings time and doesn't ever change seasons from summer over the span of 364 days. sometimes people call it arizona."
+end
+
+def ca
+  "we're landing in the sunshiney coast of california!! enjoy your stay!!"
+end
+
+def fasten_seatbelts
+  "you BEST be fastening your seatbelts right now before you bust a hip."
+end
+
+def free_to_move
+  "okay, folks. we're flying high like a g6. you can move around now, no worries, fam."
+end
+
+puts fasten_seatbelts
+puts ny
+puts pa
+puts oh
+puts inn.swapcase.reverse
+puts il
+puts ms.upcase
+puts free_to_move
+puts ks
+puts fasten_seatbelts
+puts co
+puts az.upcase
+puts ca
